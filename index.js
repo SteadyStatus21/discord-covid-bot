367eb1b7dc3647dce96ac2bc034c328fea182823
SteadyStatus21 <73841304+SteadyStatus21@users.noreply.github.com>
2022-01-04 10:27:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 const client = new CommandoClient({
 	commandPrefix: '?',
-	owner: '772641441582678076',
-	invite: 'https://discord.gg/J8HVd9fj',
+	owner: '492136745475899440',
+	invite: 'https://discord.gg/JbmucBKFuk',
 });
 
 
